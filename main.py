2edd8c5099cc957580591ec5e51845cc02c29a42
root <admin@example.com>
2022-06-30 12:35:31 +1200

    made it tell me to go to brazil

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ if aFloat > 36:
     print('my name is jeff!')
 elif aFloat < 36:
     print('get out small child')
+print('where do you live')
+address = input()
+print('you live in', address)
+if address == 'no':
+    print('you are going to brazil and there is nothing you can do about it')
+elif address == 'yes':
+    print('you are going to brazil and there is nothing you can do about it')
+else: print("you are not going to brazil and there is nothing you can do about it")
