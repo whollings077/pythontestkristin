d4660427698e8b1d53b9aa6fd9ed6720b8e5f5a8
root <admin@example.com>
2022-06-30 12:20:09 +1200

    inital commit

---
+++ main.py
@@ -0,0 +1,8 @@
+inputString = input()
+aFloat = float(inputString)
+print(aFloat + 35)
+print('Second line:', input())
+if aFloat > 36:
+    print('my name is jeff!')
+elif aFloat < 36:
+    print('get out small child')
