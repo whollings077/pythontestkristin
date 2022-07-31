ec324986dd8368cdf519dff23aa67cc60a7f7af0
root <admin@example.com>
2022-08-01 11:36:37 +1200

    made it draws stuff

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-inputString = input()
-aFloat = float(inputString)
-print(aFloat + 35)
-print('Second line:', input())
-if aFloat > 36:
-    print('my name is jeff!')
-elif aFloat < 36:
-    print('get out small child')
-print('where do you live')
-address = input()
-print('you live in', address)
-if address == 'no':
-    print('you are going to brazil and there is nothing you can do about it')
-elif address == 'yes':
-    print('you are going to brazil and there is nothing you can do about it')
-else: print("you are not going to brazil and there is nothing you can do about it")
+pythonangle = 1
+fowardamount = 50
+import turtle
+from turtle import *
+color('red', 'yellow')
+turtle.speed(0)
+turtle.pensize(10)
+while True:
+    turtle.forward(20)
+    turtle.left(35)
+
+done()
