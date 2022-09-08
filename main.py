9b8928adf072a667ec3a005d8903f537d14100d8
root <admin@example.com>
2022-09-08 15:03:36 +1200

    added random number generation, answer handling and logfile gen

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 from tkinter import *
+import random
 class MyWindow:
     def __init__(self, win):
 
-        self.lbl1=Label(win, text='First number')
-        self.lbl2=Label(win, text='Second number')
         self.lbl3=Label(win, text='Result')
         self.lbl4=Label(win, text='Answer')
         self.t1=Entry(bd=3)
         self.t2=Entry()
         self.t3=Entry()
         self.t4=Entry()
-        self.btn1 = Button(win, text='Add')
-        self.btn2=Button(win, text='Subtract')
-        self.lbl1.place(x=100, y=50)
-        self.t1.place(x=200, y=50)
-        self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
-        self.b1=Button(win, text='Add', command=self.add)
+        self.t1.place(x=50, y=50)
+        self.t2.place(x=250, y=50)
+        self.b1=Button(win, text='submit answer', command=self.answer)
+        self.b2=Button(win, text='generate numbers', command=self.generate)
         self.b1.place(x=100, y=150)
+        self.b2.place(x=200, y=150)
         self.lbl3.place(x=100, y=200)
         self.t3.place(x=200, y=200)
         self.lbl4.place(x=100, y=250)
         self.t4.place(x=200, y=250)
-    def add(self):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=int(self.t4.get())
-        if num1+num2==result:
-            self.t3.insert(END, 'Correct')
+    def generate(self):
+        self.t1.delete(0, 'end')
+        self.t2.delete(0, 'end')
+        self.t4.delete(0, 'end')
+        num1=random.randint(1, 10)
+        num2=random.randint(1, 10)
+        self.t1.insert(END, str(num1))
+        self.t2.insert(END, str(num2))
+
+    def answer(self):
+        answer=int(self.t3.get())
+        result=int(self.t1.get())+int(self.t2.get())
+        if result==answer:
+            self.t4.delete(0, 'end')
+            self.t4.insert(END, 'Correct')
             with open('logs.txt', 'a') as f:
-                f.write("Correct " + str(num1) + '+' + str(num2) + '=' + str(num1+num2) + '\n' )
+                f.write("Correct " + str(answer) + "="+ str(result) + '\n')
         else:
-            self.t3.insert(END, 'Incorrect')
+            self.t4.delete(0, 'end')
+            self.t4.insert(END, 'Incorrect')
             with open('logs.txt', 'a') as f:
-                f.write("Incorrect " + str(num1) + '+' + str(num2) + '=' + str(num1+num2) + '\n')
+                f.write("Incorrect " + str(answer) + "="+ str(result) + '\n')
 
 window=Tk()
 mywin=MyWindow(window)
