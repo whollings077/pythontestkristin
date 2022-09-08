de55ac026c861fb01b15744b52b7ae9b502aa655
root <admin@example.com>
2022-09-08 12:24:47 +1200

    made it do maths and added gui

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-pythonangle = 1
-fowardamount = 50
-import turtle
-from turtle import *
-color('red', 'yellow')
-turtle.speed(0)
-turtle.pensize(10)
-while True:
-    turtle.forward(20)
-    turtle.left(35)
+from tkinter import *
+class MyWindow:
+    def __init__(self, win):
+        self.lbl1=Label(win, text='First number')
+        self.lbl2=Label(win, text='Second number')
+        self.lbl3=Label(win, text='Result')
+        self.t1=Entry(bd=3)
+        self.t2=Entry()
+        self.t3=Entry()
+        self.btn1 = Button(win, text='Add')
+        self.btn2=Button(win, text='Subtract')
+        self.lbl1.place(x=100, y=50)
+        self.t1.place(x=200, y=50)
+        self.lbl2.place(x=100, y=100)
+        self.t2.place(x=200, y=100)
+        self.b1=Button(win, text='Add', command=self.add)
+        self.b1.place(x=100, y=150)
+        self.lbl3.place(x=100, y=200)
+        self.t3.place(x=200, y=200)
+    def add(self):
+        self.t3.delete(0, 'end')
+        num1=int(self.t1.get())
+        num2=int(self.t2.get())
+        result=5
+        if num1+num2==result:
+            self.t3.insert(END, 'Correct')
+        else:
+            self.t3.insert(END, 'Incorrect')
 
-done()
+window=Tk()
+mywin=MyWindow(window)
+window.title('Hello Python')
+window.geometry("400x300+10+10")
+window.mainloop()
